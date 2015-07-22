acd5bbd95880af1ac08103f06cd179e18d756740
Mitchell Kember <mk12360@gmail.com>
2015-07-21 22:01:23 -0400

    Look for merge button class, not text
    The text "This pull request can be automatically merged" no longer appears after
    GitHub changes, and checking for a specific message is too brittle anyway.

--- inject.js
+++ inject.js
@@ -1,8 +1,8 @@
 (function() {
 	var counter = document.getElementById('commits_tab_counter');
-	var merge = document.getElementById('partial-pull-merging');
-	var canMerge = "This pull request can be automatically merged.";
-	var stillOpen = merge.innerHTML.indexOf(canMerge) !== -1;
+	var mergeArea = document.getElementById('partial-pull-merging');
+	var buttonClass = 'js-merge-branch-action';
+	var stillOpen = mergeArea.innerHTML.indexOf(buttonClass) !== -1;
 	var multipleCommits = counter.innerHTML.trim() !== '1';
 
 	if (stillOpen && multipleCommits) {
